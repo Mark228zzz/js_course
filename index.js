b1599a30dad80c55b3dccba02aec6f32bed38087
Mark <mark.mazur007@gmail.com>
2021-07-14 18:23:01 +0300

    function

--- index.js
+++ index.js
@@ -135,12 +135,65 @@
 
 // alert('Your number is: ' + z)
 
-const isReady = true
+// const isReady = true // true or false
 
-if(isReady) {
-    console.log('ok')
-} else {
-    console.log('error')
+// if(isReady) {
+//     console.log('ok')
+// } else {
+//     console.log('error')
+// }
+
+// isReady ? console.log('ok') : console.log('error')
+
+
+// functions
+
+// const number = 100
+
+// function calculate1(myAge){
+//     return number - myAge
+// }
+// function calculate2(sisterAge){
+//     return number - sisterAge
+// }
+
+// // const result1 = calculate1(35)
+// // const result2 = calculate2(40)
+// // console.log(result1)
+// // console.log(result2)
+
+// console.log(calculate1(-10)) // 110
+// console.log(calculate1(40)) // 60
+// console.log(calculate1(0)) // 100
+
+// console.log(calculate1(-10) + calculate2(-20)) // 230
+// console.log(calculate1(40) + calculate2(70)) // 90
+// console.log(calculate1(0) + calculate2(0)) // 200
+// console.log(calculate1(0) + calculate2(-3)) // 203
+// console.log(calculate1(0) + calculate2(8)) // 192
+// console.log(calculate1(0) / calculate2(0)) // 1
+
+
+const number = 1000
+
+function calculateAge(myAge){
+    return number - myAge
+}
+function fullName(firstName , lastName){
+    return '.My full name is ' + firstName + lastName
 }
 
-isReady ? console.log('ok') : console.log('error')
+function info(firstName, lastName){
+    const age = calculateAge(35)
+    if (age < 0) {
+        console.log('age cant be less then zerro')
+    } else if (age > 90) {
+        console.log('yor older man')
+    }
+    const name = fullName(firstName, lastName)
+    console.log('My age: ' + age + name)
+}
+
+info('Mark', ' Mazur')
+info('Masha', ' red')
+info('dasha', ' green')
