149ec8b9220e22adda31148e6dabdaa7d2c02dde
Mark <mark.mazur007@gmail.com>
2021-07-14 15:18:02 +0300

    new calculator and operator

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // 1 variables
 
-const firstName = '"Sasha"'
-const lastName = 'San'
-let age = 35
-const isValid = false
-const isNoValid = true
-const _name = 'Mark'
-const $ = 250
+// const firstName = '"Sasha"'
+// const lastName = 'San'
+// let age = 35
+// const isValid = false
+// const isNoValid = true
+// const _name = 'Mark'
+// const $ = 250
 
-age = '37'
+// age = '37'
 
 // console.log(age)
 // console.log(isValid)
@@ -54,42 +54,93 @@ age = '37'
 
 // alert('Your number is: ' + z)
 
-let x = 6
-let y = 3
+// let x = 6
+// let y = 3
 
-console.log(x += y)
+// console.log(x += y)
 
-x = 6
-y = 3
+// x = 6
+// y = 3
 
-console.log(x -= y)
+// console.log(x -= y)
 
-x = 6
-y = 3
+// x = 6
+// y = 3
 
-console.log(x *= y)
+// console.log(x *= y)
 
-x = 6
-y = 3
+// x = 6
+// y = 3
 
-console.log(x /= y)
+// console.log(x /= y)
 
 // 3 data types 
 
-const boolean = true
-const string = 'Sasha'
-const number = 35
-let p
-let a = null
-const array = []
-const obj = {}
-const bigInt = sd12341312313132141241244141341324152535n
+// const boolean = true
+// const string = 'Sasha'
+// const number = 35
+// let p
+// let a = null
+// const array = []
+// const obj = {}
+// const bigInt = 12341312313132141241244141341324152535n
 
-console.log(typeof boolean)
-console.log(typeof string)
-console.log(typeof number)
-console.log(typeof p)
-console.log(typeof a)
-console.log(typeof array)
-console.log(typeof obj)
-console.log( bigInt)
+// console.log(typeof boolean)
+// console.log(typeof string)
+// console.log(typeof number)
+// console.log(typeof p)
+// console.log(typeof a)
+// console.log(typeof array)
+// console.log(typeof obj)
+// console.log( bigInt)
+
+// console.log(1 === '1')
+// console.log(1 == '1')
+// console.log(1 === 1)
+
+// console.log(1 != 3)
+// console.log(1 != '3')
+// console.log(1 != '1')
+// console.log(1 !== 3)
+
+// const color = 'red' // red black green
+
+// if (color === 'red') {
+//     console.log('color red')
+// } else if (color === 'black') {
+//     console.log('color black')
+// } else {
+//     console.log('color empty')
+// }
+
+// x = prompt('Type first numver')
+// y = prompt('Type second numver')
+// oper = prompt('Type an operator')
+
+// if(oper=='') {
+//     z = 'Error 3'
+// } else if (x == '') {
+//     z = 'Error 1'
+// } else if (y == '') {
+//     z = 'Error 2'
+// } else if(oper=='-') {
+//     z = Number(x) - Number(y)
+// } else if(oper=='/') {
+//     z = Number(x) / Number(y)
+// } else if(oper=='*') {
+//     z = Number(x) * Number(y)
+// } else if (oper == '+') {
+//     z = Number(x) + Number(y)
+// }
+
+// alert('Your number is: ' + z)
+
+const isReady = true
+
+if(isReady) {
+    console.log('ok')
+} else {
+    console.log('error')
+}
+
+isReady ? console.log('ok') : console.log('error')
