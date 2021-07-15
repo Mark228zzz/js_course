a83c3437775ac21def2eeb3777322e216b91504e
Mark <mark.mazur007@gmail.com>
2021-07-15 13:39:54 +0300

    update functions.js

--- functions.js
+++ functions.js
@@ -62,14 +62,43 @@
 // console.log(calculate(10)) // 50
 // console.log(calculate(10, 8))// 80, z = 5 --> z = 8 - default 
 
-const calculate1 = (...all) => console.log(all)
-const calculate = (...all) => all.reduce((sum, a) => sum + a, 0)
+// const calculate1 = (...all) => console.log(all)
+// const calculate = (...all) => all.reduce((sum, a) => sum + a, 0)
 
-calculate1(1, 2, 3, 4, 5, 6) // calculate(1, 2...6) - Array(6)
-console.log(calculate(1, 2, 3, 4, 5, 6)) // 21
+// calculate1(1, 2, 3, 4, 5, 6) // calculate(1, 2...6) - Array(6)
+// console.log(calculate(1, 2, 3, 4, 5, 6)) // 21
 
-function calculate2(...data) {
-    return data.reduce((b, a) => b + a, 0)
-}
+// function calculate2(...data) {
+//     return data.reduce((b, a) => b + a, 0)
+// }
 
-console.log(calculate2(1, 2, 3, 4, 5, 6)) // 21
+// console.log(calculate2(1, 2, 3, 4, 5, 6)) // 21
+
+// function getName(name) {
+//     return function(lastName = ' empty ') {
+//         console.log(name + lastName)
+//     }
+// }
+
+// const login = getName(' Mark')
+// console.log(login(' Mazur')) // Mark Mazur
+// console.log(login(' Dev')) // Mark Dev
+// console.log(login( )) // Mark empty
+
+// function splitString(stringToSplit, separetor) {
+//     const arrayOfStrings = stringToSplit.split(separetor) 
+
+//     console.log('Original string: ' + stringToSplit)
+//     console.log('Seporator: ' + separetor)
+//     console.log('Array: ' + arrayOfStrings.lenght + ' elements: ' + arrayOfStrings.join(' / '))
+// }
+
+// const tempestString = 'и как хорош тот новый мир, где есть такие люди!'
+// const monthString = 'January,February,March,April,May,June,July,August,September,October,November,December'
+
+// const space = ' '
+// const comma = ','
+
+// splitString(tempestString, space)
+// splitString(tempestString)
+// splitString(monthString, comma)
