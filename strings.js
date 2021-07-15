edbfd98f01defbd6ccc96d132a8991b4c5bb0ad2
Mark <mark.mazur007@gmail.com>
2021-07-15 10:43:26 +0300

    new file strings.js

---
+++ strings.js
@@ -0,0 +1,54 @@
+
+// // strings
+
+// const firstName = 'Mark'
+
+// function getDOB(DOB){
+//     return DOB
+// }
+
+// const myInfo1 = 'My name is: ' + firstName + ' ' + 'and my DOB is: ' + getDOB(2007)
+// const myInfo2 = `My name is: ${firstName} and my DOB is: ${getDOB(2006) < 2006 ? 'less then 2007' : 'more then 2007'}`
+// const myInfo3 = `My name is: ${firstName} and my DOB is: ${getDOB(2006) < 2006 ? 'less then 2007' : 'more then 2007'}`
+
+// const myInfo4 = `<h1> My name is ${firstName} </h1>
+// <p> and DOB is ${getDOB(2007)} </p>
+// `
+
+
+
+// console.log(myInfo1)
+// console.log(myInfo2)
+// console.log(myInfo3)
+// console.log(myInfo4)
+
+// const firstName = ' Mark '
+
+// console.log(firstName.length)
+// console.log(firstName.toUpperCase()) // MARK
+// console.log(firstName.toLowerCase()) // mark
+
+// console.log(firstName.charAt(0)) // M
+// console.log(firstName.charAt(1)) // a
+// console.log(firstName.charAt(2))//  r
+// console.log(firstName.charAt(3)) // k
+
+// console.log(firstName.indexOf('ma')) // -1 
+// console.log(firstName.indexOf('Ma')) // 1
+
+// console.log(firstName.startsWith('Mar')) // true
+// console.log(firstName.startsWith('mar')) // false
+// console.log(firstName.startsWith('MAr')) // false
+
+// console.log(firstName.endsWith('rk')) // true
+// console.log(firstName.endsWith('rK')) // false
+// console.log(firstName.endsWith('Rk')) // false
+// console.log(firstName.endsWith(' rk')) // false
+// console.log(firstName.endsWith('rk ')) // false
+
+// console.log(firstName.repeat(2)) //  Mark Mark
+// console.log(firstName.repeat(6)) //  Mark Mark Mark Mark Mark Mark
+
+// console.log(firstName.trim()) // Mark
+// console.log(firstName.trimLeft()) // Mark 
+// console.log(firstName.trimRight()) //  Mark
