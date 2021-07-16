e67e9ddbe63d3ac2f01eb011d80e97af15b7fbf3
Mark <mark.mazur007@gmail.com>
2021-07-16 11:46:14 +0300

    new file arrays.js

---
+++ arrays.js
@@ -0,0 +1,57 @@
+
+// arrays
+
+const array1 = ['Mark', 'Mazur', 'Dasha', 'Masha']
+const array2 = [1, 2, 3, 4, 5, 6]
+const array3 = [true, false, false, true]
+const array4 = [['M', 'a', 'r', 'k'], [true, false, false, true], [1, 2, 3, 4, 5, 6], [true, 1, 1000000, 'sdagdfsfwsefe']]
+const array5 = [{'M':'a'}, {'r':'k'}, {'M':'a'}, {'r':'k'}]
+
+// // add olia in array1
+// // push - add at the end array
+// array1.push('olia')
+
+// // delete zero index in array1
+// // shift - delete zero index
+// const first = array1.shift()
+
+// // add kosta in array1
+// // unshift - add at the start array
+// array1.unshift('kosta')
+
+// // removes the end of the array1
+// // pop = removes the end
+// const last = array2.pop()
+
+// console.log(array2.pop())
+// console.log(array2)
+
+// console.log('delete', last)
+// console.log('delete', first)
+
+// // array mirrored
+// console.log(array1.reverse())
+
+// const text = 'my name is Mark and i am full stack dev'
+// const revers = text.split("").reverse().join('') // ved kcats lluf ma i dna kraM si eman ym
+
+// array1.forEach(function(item, index) {
+//     console.log(item, index) // Mark - 0   // Mazur - 1   // Dasha - 2   // Masha - 3 n+
+//     console.log(array1)
+// })
+
+// console.log(revers)
+
+// console.log(array1.indexOf('Mark')) //  0
+// console.log(array1.indexOf('Mazur')) // 1
+// console.log(array1.indexOf('Dasha')) // 2
+// console.log(array1.indexOf('Masha')) // 3
+
+// removeItem = array1.splice(1, 1) // remove in array1 Mazur
+// console.log(array1)
+
+// const x = array1 // copy array1
+
+// console.log(x)
+// console.log(array1)
+
