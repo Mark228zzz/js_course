8f1b078b6cd3a40b831f6f02f9da7511e73e97b0
Mark <mark.mazur007@gmail.com>
2021-08-02 22:47:24 +0300

    update folder mymark

--- mymark/app.js
+++ mymark/app.js
@@ -1,11 +1,29 @@
 const App = {
     data() {
         return {
+            h: 0,
+            counter: 0,
             placeholderString: 'name your note',
             title: 'list notes',
             inputValue: '',
-            notes: []
-
+            notes: [],
+            message: '',
+            message2: '',
+            checked1: 'I need an answer',
+            checked2: 'I need an answer',
+            checkedNames: [],
+            picked: '',
+            selected: '',
+            selected2: '',
+            selected3: '',
+            options: [
+                { text: 'one', value: 'three' },
+                { text: 'two', value: 'one' },
+                { text: 'three', value: 'two' },
+                { text: 'what', value: 'wait' }
+            ],
+            currentTab: 'Home',
+            tabs: ['Home', 'Posts', 'Archive']
         }
     },
     methods: {
